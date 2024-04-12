d5d936160a037ce99ccbd848b52efe0257da9d69
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-04-12 10:02:52 +0700

    finish

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = "User1";
+        $user->email =  "user@gmail.com";
+        $user->password = Hash::make("123456");
+        $user->save();
+    }
+}
